8c3a4410419502ee6bb354cbcda006f88ad6afed
ksmielke <ksmielke@mymail.mines.edu>
2019-11-21 20:16:11 -0700

    added function 2

--- Week12a-utility.py
+++ Week12a-utility.py
@@ -1,3 +1,21 @@
+ #   incremental build model 
+#   kimberly mielke
+ #  ​CSCI 102 – Section A 
+#   Week 12 - Part A 
+
+
+
 #1
 def PrintOutput(x):
     print('OUTPUT', x)
+
+#2
+def LoadFile(x):
+    lis = []
+    txt = open(x, 'r')
+    #print(txt)
+    doc = txt.readlines()
+    #print(doc)
+    for n in doc:
+        lis.append(n.strip())
+    return (lis)
